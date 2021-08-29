ea08cfa264596dc6993d3be4c4128d52a6c8129b
Gabor Puskas <puskas.gabor.zeusz@gmail.com>
2021-08-29 10:16:51 +0200

    Refractored client side encryption

--- static/js/crypto.js
+++ static/js/crypto.js
@@ -5,17 +5,12 @@ $('#sensitive').submit((event) => {
 })
 
 function encryptForm(crypto) {
-    switch(window.location.pathname) {
-        case '/login/':
-            encryptLogin(crypto)
-            break
-        case '/register/':
-            encryptRegistration(crypto)
-            break
-        case '/user/manage/':
-            encryptManage(crypto)
-            break
-    }
+    const form = document.getElementById('sensitive')
+    Array.from(form.getElementsByTagName('input')).forEach(input => {
+        if(input.type == 'password') {
+            input.value = crypto.encrypt(input.value)
+        }
+    });
 }
 
 function RSAEncrypt() {
@@ -26,7 +21,7 @@ function RSAEncrypt() {
     xhttp.setRequestHeader('X-CSRFToken', shortcuts.getCSRFToken())
     
     xhttp.onreadystatechange = () => {
-        if(xhttp.readyState == 4) {
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
             // key retrieved, encrypt credentials
             const crypto = new JSEncrypt()
             crypto.setKey(xhttp.response)
@@ -37,28 +32,4 @@ function RSAEncrypt() {
         }
     }
     xhttp.send()
-}
-
-function encryptLogin(crypto) {
-    const password = document.getElementsByName('password')[0]
-    password.value = crypto.encrypt(password.value)
-}
-
-function encryptRegistration(crypto) {
-    const password1 = document.getElementsByName('password1')[0]
-    password1.value = crypto.encrypt(password1.value)
-
-    const password2 = document.getElementsByName('password2')[0]
-    password2.value = crypto.encrypt(password2.value)
-}
-
-function encryptManage(crypto) {
-    const oldPassword = document.getElementsByName('old_password')[0]
-    oldPassword.value = crypto.encrypt(oldPassword.value)
-
-    const newPassword1 = document.getElementsByName('new_password1')[0]
-    newPassword1.value = crypto.encrypt(newPassword1.value)
-    
-    const newPassword2 = document.getElementsByName('new_password2')[0]
-    newPassword2.value = crypto.encrypt(newPassword2.value)
 }
